5075870c5adc4bb77501aedc1d0733eb81ced43a
azizx018 <chiara.aziz@gmail.com>
2023-09-29 11:30:33 -0500

    redo

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,50 @@
+import React from "react"
+
+
+type Props = TodoProps & {
+    updateTodo: (todo: ITodo) => void
+    deleteTodo: (_id: string) => void
+    saveTodo: (e: React.FormEvent, formData: ITodo) => void
+
+}
+
+const Todo: React.FC<Props> = ({ todo, updateTodo, deleteTodo, saveTodo }) => {
+    const checkTodo: string = todo.status ? `line-through` : ""
+    return (
+        <div className="Card">
+            <div className="Card--text">
+                <h1 className={checkTodo}>{todo.name}</h1>
+                <span className={checkTodo}>{todo.description}</span>
+            </div>
+
+            <div className="Card--button">
+                <button
+                    onClick={() => updateTodo(todo)}
+                    className={todo.status ? `hide-button` : "Card--button__done"}
+                >
+                    Complete
+                </button>
+                <button
+                    onClick={() => deleteTodo(todo._id)}
+                    className="Card--button__delete"
+                >
+                    Delete
+
+                </button>
+                {/* <button
+                    onClick={() => saveTodo(todo)}
+                    className="Card--button"
+                >
+                    Add Todo
+
+                </button> */}
+
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Todo
